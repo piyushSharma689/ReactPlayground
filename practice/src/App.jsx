6762a154f86d72bffb0381aadb0e4e325d6a34e3
Piyush Vishwakarma <piyushvishwakarma6706@gmail.com>
2024-05-21 14:09:18 +0530

    practice start

--- practice/src/App.jsx
+++ practice/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import ArrayState from "./Array";
 
 import "./App.css"
 // function App(){
@@ -36,62 +36,9 @@ import "./App.css"
 
 
 function App(){
-   const [arr, setarr]= useState([""]);
-   const [value, setvalue]=useState("");
- 
-  const AddElement =()=>{
-    arr.push(value);
-    setarr([...arr]);
-    setvalue("");   
-  }
-  const PopElement=()=>{
-    arr.pop();
-    setarr([...arr]);
-  }
-  const Addstart=()=>{
-    arr.unshift(value)
-    setarr([...arr])
-    setvalue("")
-  }
-  const ClearArr=()=>{
-      setarr([])
-  }
-  const RemoveElement=()=>{
-    
-    setarr(arr.filter(item=> item !== value));
-    setvalue('');
-  }
-
-  const PopFirst=()=>{
-    arr.shift();
-    setarr([...arr  ])
-  }
-
-  const handleKey=(e)=>{
-    if(e.key ==="Enter"){
-      AddElement();
-    }
-  }
-
-return(<>
-<center>
-<h1>ARRAY STATE PRACTICE</h1>
-<p>Added on github</p>
-<hr />
-  <input type="text" value={value} onKeyDown={handleKey} onChange={e=>{
-    setvalue(e.target.value);
-  }}/>
-  <hr />
-  <br />
-  <button onClick={Addstart}>AddStart</button>
-  <button onClick={AddElement}>push</button>
-  <button onClick={PopElement}>Pop</button>
-  <button onClick={PopFirst}>PopFirst</button>
-  <button onClick={ClearArr}>clear</button>
-  <button onClick={RemoveElement}>remove</button>
-
-  <h1>{"Array = ["+arr.join(", ")+"]"}</h1></center>
-  </>);
+   return(<>
+   <ArrayState></ArrayState>
+   </>);
 }
 
 
